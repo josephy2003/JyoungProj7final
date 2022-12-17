d44017bdde3e24175e77ad2c1585283b8b368198
_ixmnxbdy111 <65754413+josephy2003@users.noreply.github.com>
2022-12-16 21:13:14 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import arcade
+import input
+
+
+greeting = ("Hello, I am your AI NPC! How may I help you...? ;):")
+intro_0 = ("Press 'S' to continue...:")
+intro_1 = ("Choose an option (1-3:")
+
+CI = COMPOUND_INTEREST
+
+    """"1.) CALCULATE COMPOUND INTEREST
+        2.) """
+
+def compound_interest(principal, rate, time):
